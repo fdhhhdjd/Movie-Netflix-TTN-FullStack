d3ba8358b2e17b0f111fd720bb4c47694fa39e56
Squirrel1610 <thinh.nd.60cntt@ntu.edu.vn>
2022-01-23 16:09:23 +0700

    Revert "Revert "admin router""
    This reverts commit d2646b2be58ecda331194ef357235c580242bb04.

---
+++ backend/Routes/adminRoute.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const userCtrl = require('../Controllers/userController.js');
+const auth = require('../middleware/auth.js');
+const authAdmin = require('../middleware/authAdmin.js');
+const router = express.Router();
+
+//đăng ký tài khoản admin
+router.post('/register', userCtrl.registerAdmin);
+
+//đăng nhập tài khoản admin
+router.post('/login', userCtrl.loginAdmin);
+
+//refresh token
+router.get('/refresh_token', userCtrl.refreshToken);
+
+//đăng xuất
+router.get('/logout', userCtrl.logout);
+
+//xem profile
+router.get('/profile', auth, authAdmin, userCtrl.profile);
+
+//cập nhập profile
+router.patch('/profile/update', auth, authAdmin, userCtrl.updateProfile);
+
+//thay đổi mật khẩu
+router.patch('/changePassword', auth, authAdmin, userCtrl.ChangePassword);
+
+//quên mật khẩu tài khoản admin
+router.post('/forget', userCtrl.forgetPasswordAdmin);
+
+//link reset mật khẩu khi quên mật khẩu
+router.put('/password/reset/:token', userCtrl.resetPassword);
+
+module.exports = router;
