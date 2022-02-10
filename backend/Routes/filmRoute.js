eb12feb2b8b3621c16685546c0155a1d73d576ce
Squirrel1610 <thinh.nd.60cntt@ntu.edu.vn>
2022-02-10 22:49:17 +0700

    update, delete and get detail film

--- backend/Routes/filmRoute.js
+++ backend/Routes/filmRoute.js
@@ -10,4 +10,16 @@ router.get('/all', auth, filmCtrl.getAllFilm);
 //Thêm thông tin bộ phim
 router.post('/add', auth, authAdmin, filmCtrl.addFilm);
 
+//Chỉnh sửa thông tin bộ phim
+router.patch('/update/:id', auth, authAdmin, filmCtrl.updateFilm);
+
+//Xóa bộ phim
+router.delete('/delete/:id', auth, authAdmin, filmCtrl.deleteFilm);
+
+//Xem thông tin chi tiết bộ phim
+router.get('/detail/:id', auth, filmCtrl.getDetailFilm);
+
+// //lấy ra những bộ phim theo thể loại
+// router.get('/find', auth, filmCtrl.getFilmByCategory);
+
 module.exports = router;
