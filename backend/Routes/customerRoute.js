cc27b7315bba570ee51031ac378317a71153a58d
Squirrel1610 <thinh.nd.60cntt@ntu.edu.vn>
2022-03-23 17:27:25 +0700

    email verify

--- backend/Routes/customerRoute.js
+++ backend/Routes/customerRoute.js
@@ -1,36 +1,39 @@
-const userCtrl = require('../Controllers/userController');
-const auth = require('../middleware/auth');
-const authCustomer = require('../middleware/authCustomer.js');
-const router = require('express').Router();
+const userCtrl = require("../Controllers/userController");
+const auth = require("../middleware/auth");
+const authCustomer = require("../middleware/authCustomer.js");
+const router = require("express").Router();
 
 //đăng ký tài khoản khách hàng
-router.post('/register', userCtrl.registerCustomer);
+router.post("/register", userCtrl.registerCustomer);
+
+//xác thực email đăng ký của khách hàng
+router.get("/verify/:userId/:uniqueString", userCtrl.verifyEmail);
 
 //đăng nhập tài khoản khách hàng
-router.post('/login', userCtrl.loginCustomer);
+router.post("/login", userCtrl.loginCustomer);
 
 //refresh token
-router.get('/refresh_token', userCtrl.refreshToken);
+router.get("/refresh_token", userCtrl.refreshToken);
 
 //đăng xuất
-router.get('/logout', userCtrl.logoutCustomer);
+router.get("/logout", userCtrl.logoutCustomer);
 
 //xem profile
-router.get('/profile', auth, authCustomer, userCtrl.profile);
+router.get("/profile", auth, authCustomer, userCtrl.profile);
 
 //cập nhập profile
-router.patch('/profile/update', auth, authCustomer, userCtrl.updateProfile);
+router.patch("/profile/update", auth, authCustomer, userCtrl.updateProfile);
 
 //thay đổi mật khẩu
-router.patch('/changePassword', auth, authCustomer, userCtrl.ChangePassword);
+router.patch("/changePassword", auth, authCustomer, userCtrl.ChangePassword);
 
 //quên mật khẩu tài khoản khách hàng
-router.post('/forget', userCtrl.forgetPasswordCustomer);
+router.post("/forget", userCtrl.forgetPasswordCustomer);
 
 //link reset mật khẩu khi quên mật khẩu
-router.put('/password/reset/:token', userCtrl.resetPassword);
+router.put("/password/reset/:token", userCtrl.resetPassword);
 
 //đăng nhập gg tài khoản khách hàng
-router.post('/loginGoogle', userCtrl.LoginGoogleCustomer);
+router.post("/loginGoogle", userCtrl.LoginGoogleCustomer);
 
 module.exports = router;
