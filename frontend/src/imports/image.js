432510afe7ea4409136b68c250bf74afdee43d63
Nguyễn Thành Tất 60136813 <63543229+nguyenthanhtat@users.noreply.github.com>
2022-04-09 22:12:10 +0700

    change fe exit kid

--- frontend/src/imports/image.js
+++ frontend/src/imports/image.js
@@ -5,4 +5,16 @@ import one from "../Image/Welcome/1.png";
 import two from "../Image/Welcome/2.png";
 import three from "../Image/Welcome/3.png";
 import four from "../Image/Welcome/4.png";
-export { logo, background, backgroundWelcome, one, two, three, four };
+import True from "../Image/true.png";
+import CheckPass from "../Image/giphy.gif";
+export {
+  logo,
+  background,
+  backgroundWelcome,
+  one,
+  two,
+  three,
+  four,
+  CheckPass,
+  True,
+};
