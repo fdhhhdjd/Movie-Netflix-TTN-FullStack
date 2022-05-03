a45c6802fe84c373c8da5e0b2a28f0486fbade1d
Nguyễn Thành Tất 60136813 <63543229+nguyenthanhtat@users.noreply.github.com>
2022-05-03 21:16:28 +0700

    fix header

--- frontend/src/Style/StyleHome/HomeStyle.js
+++ frontend/src/Style/StyleHome/HomeStyle.js
@@ -1,6 +1,7 @@
 import { createGlobalStyle } from "styled-components";
 
 export const HomeStyle = createGlobalStyle`
+
 .home{
     background-color: #141414;
     overflow: hidden;
@@ -15,4 +16,3 @@ export const HomeStyle = createGlobalStyle`
     bottom: 0;
 }
 `;
-
