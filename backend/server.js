12141321c1f178ac56a395c281d0e8af56fdf8fe
Squirrel1610 <thinh.nd.60cntt@ntu.edu.vn>
2022-05-27 00:47:06 +0700

    fix

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,5 @@ app.get("/", (req, res) => {
   res.json({ message: "Welcome to Tai heo Fa" });
 });
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () =>
-  console.log(`server is listening on port:http://localhost:${PORT}`)
-);
+const PORT = 3000 || process.env.PORT;
+app.listen(PORT, () => console.log(`server is listening `));
